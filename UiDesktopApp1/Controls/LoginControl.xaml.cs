99de425a136bb4118aa534f305de931461c17f98
ddani-commits <23090584@alumno.utmetropolitana.edu.mx>
2025-06-02 00:30:47 -0600

    auth views

---
+++ UiDesktopApp1/Controls/LoginControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+using UiDesktopApp1.Services;
+
+namespace UiDesktopApp1.Controls
+{
+    public partial class LoginControl : UserControl
+    {
+        private IAuthenticationService _authenticationService;
+        private string _email = string.Empty;
+        private string _password = string.Empty;
+
+        public LoginControl()
+        {
+            InitializeComponent();
+        }
+
+        public void SetAuthenticationService(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            _authenticationService.HasUsers();
+            _authenticationService.Login(_email, _password);
+        }
+    }
+}
---
+++ UiDesktopApp1/Controls/RegisterControl.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using UiDesktopApp1.Services;
+
+namespace UiDesktopApp1.Controls
+{
+    public partial class RegisterControl : UserControl
+    {
+        private IAuthenticationService _authenticationService;
+
+        private string _firstName = "";
+        private string _lastName = "";
+        private string _email = "";
+        private string _password = string.Empty;
+        private string _confirmPassword = string.Empty;
+
+        public string FirstName { 
+            get => _firstName; 
+            set { _firstName = value; OnPropertyChanged(); } 
+        }
+        public string LastName
+        {
+            get => _lastName;
+            set { _lastName = value; OnPropertyChanged(); }
+        }
+        public string Email
+        {
+            get => _email;
+            set { _email = value; OnPropertyChanged(); }
+        }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+        public RegisterControl() 
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public RegisterControl(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public void SetAuthenticationService(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e) {
+            Debug.WriteLine(FirstName);
+            Debug.WriteLine(LastName);
+            Debug.WriteLine(Email);
+            _authenticationService.Register(FirstName, LastName, Email);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // Notify property changes for data binding
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
